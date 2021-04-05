e792db0aa072a00fb2af3697df4b7a1bfc21b03b
Thibaut De Maerteleire <thibaut.demaerteleire@dukeandgrace.com>
2021-04-05 14:05:36 +0200

    Added storybook and scully

---
+++ client/src/app/components/form/input-currency/input-currency.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-currency',
+  templateUrl: './input-currency.component.html',
+  styleUrls: ['./input-currency.component.scss']
+})
+export class InputCurrencyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
