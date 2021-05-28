462ac962be440b758df89fe58983bef1dda935fc
thibautdm9830 <thibautdm9830@hotmail.com>
2021-05-28 10:10:05 +0200

    test

--- server/app/Http/Middleware/VerifyCsrfToken.php
+++ server/app/Http/Middleware/VerifyCsrfToken.php
@@ -13,7 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        // 'localhost',
+        '*',
         // 'thibautdemaerteleire.be',
         // 'footy.thibautdemaerteleire.be'
     ];
