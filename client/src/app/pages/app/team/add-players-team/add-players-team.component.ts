9d3528440fc48ffc86581487d68624d9723c2f48
thibautdm9830 <thibautdm9830@hotmail.com>
2021-06-11 00:59:07 +0200

    Need to add http request

--- client/src/app/pages/app/team/add-players-team/add-players-team.component.ts
+++ client/src/app/pages/app/team/add-players-team/add-players-team.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, } from '@angular/core';
+import { IPlayer } from 'src/app/widgets/add-players/add-players.component';
 
 @Component({
   selector: 'app-add-players-team',
   templateUrl: './add-players-team.component.html',
   styleUrls: ['./add-players-team.component.scss']
 })
-export class AddPlayersTeamComponent implements OnInit {
+export class AddPlayersTeamComponent {
 
-  constructor() { }
+  players: IPlayer[] = [];
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) { }
+
+  submit(): void {
+    const checkPlayers: IPlayer | undefined = this.players?.find((e: IPlayer) => !e.function);
+
+    if (checkPlayers) {
+      return;
+    }
+
+    // Add http request
+
+    return;
   }
 
 }
